d2ead2a21038d7a46acf38d3e302e03e5d3418fa
ercumenlacin <1140104023@nku.edu.tr>
2020-11-01 20:27:36 +0300

    deneme

---
+++ index.js
@@ -0,0 +1,12 @@
+const adviceArea = document.querySelector(".advice-slip");
+
+function getAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then(
+        (data) =>
+          (adviceArea.innerHTML = `<h2 class="advice">${data.slip.advice}</h2>`)
+      );
+  }
+
+getAdvice();
